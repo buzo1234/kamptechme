a89d4235c52c85b5c1f13b6282e6c77b89e2b8bb
Karan Dua <karandua2002@gmail.com>
2023-10-11 18:53:12 +0530

    Update index.tsx

--- src/components/HeroImageRight/index.tsx
+++ src/components/HeroImageRight/index.tsx
@@ -164,7 +164,7 @@ export function HeroImageRight() {
               src='https://appwrite.techsouqdubai.com/v1/storage/buckets/647724cbbee87d2e8f59/files/64772506bd1d1a8ea931/view?project=646339a61beac87efd09&mode=admin' loading='lazy'
               alt='banner1'
               onLoad={handleImageLoad}
-              className='h-full aspect-auto object-center w-full object-contain '
+              className='h-full aspect-auto object-center w-full object-fill '
             />}
            
           </div>
